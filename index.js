888caa767b7ad0fba1dad217db252e60f0291b85
david1opez <david1opez@outlook.com>
2023-11-07 15:33:42 -0600

    Panic

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, update, onValue } from "firebase/database";
+import mysql2 from 'mysql2'
 
 const app = express();
 const port = 3000;
@@ -10,45 +8,75 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBclDs5GhHaouQ4YgDb3Xmd7p-dLI8IBAY",
-    authDomain: "iotdb-e823a.firebaseapp.com",
-    projectId: "iotdb-e823a",
-    storageBucket: "iotdb-e823a.appspot.com",
-    messagingSenderId: "841356106534",
-    appId: "1:841356106534:web:5316ca365c6a646503bfe7",
-    databaseURL: "https://iotdb-e823a-default-rtdb.firebaseio.com",
-};
+const db = mysql2.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '@Inteligente2004',
+    database: 'iotproyecto',
+});
+  
+// Connect to MySQL
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL: ' + err.stack);
+        return;
+    }
+    console.log('Connected to MySQL as id ' + db.threadId);
+});
 
-const fireApp = initializeApp(firebaseConfig);
-
-app.get('/sensors', (req, res) => {
-    const db = getDatabase();
-
-    const starCountRef = ref(db);
-    onValue(starCountRef, (snapshot) => {
-        const data = snapshot.val();
-        res.status(200);
-        res.send(data);
+app.get('/getLugar', (req, res) => {
+    db.query('SELECT * FROM lugar', (error, results) => {
+        if (error) throw error;
+        res.json(results);
     });
 });
 
-app.post('/sensors', (req, res) => {
-    const db = getDatabase();
-
-    let data = req.body;
-
-    update(ref(db), {
-        alt: data.alt,
-        x: data.x,
-        y: data.y,
-        tilt: data.tilt
-    }).then(() => {
-        res.status(200);
-        res.send('Data Received: ' + JSON.stringify(data));
+// Route: getEficiencia
+app.get('/getEficiencia', (req, res) => {
+    db.query('SELECT * FROM eficiencia', (error, results) => {
+        if (error) throw error;
+        res.json(results);
     });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+// Route: getSensores
+app.get('/getSensores', (req, res) => {
+    db.query('SELECT * FROM sensores', (error, results) => {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+// Route: getSensor?fecha
+app.get('/getSensor', (req, res) => {
+    const fecha = req.query.fecha;
+    db.query('SELECT * FROM sensores WHERE fecha_hora = ?', [fecha], (error, results) => {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+// Route: setSensor
+app.post('/setSensor', (req, res) => {
+    const { temperatura, humedad, orientacion, luz, id_lugar, fecha_hora } = req.body;
+    db.query('INSERT INTO sensores (temperatura, humedad, orientacion, luz, id_lugar, fecha_hora) VALUES (?, ?, ?, ?, ?, ?)',
+        [temperatura, humedad, orientacion, luz, id_lugar, fecha_hora],
+        (error, results) => {
+            if (error) throw error;
+            res.json({ message: 'Sensor data inserted successfully' });
+        }
+    );
+});
+
+// Route: setLugar
+app.post('/setLugar', (req, res) => {
+    const { latitud, longitud } = req.body;
+
+    db.query('INSERT INTO lugar (latitud, longitud) VALUES (?, ?)',
+        [latitud, longitud],
+        (error, results) => {
+            if (error) throw error;
+            res.json({ message: 'Lugar data inserted successfully' });
+        }
+    );
 });
